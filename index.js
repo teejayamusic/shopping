0fa552a96cec3d23e28d5c93d96d194e65e4fa3b
teejayamusic <142833479+teejayamusic@users.noreply.github.com>
2024-04-02 12:51:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,14 +8,13 @@ const jwt = require('jsonwebtoken');
 
 const secretKey = 'hello1234';
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   connectionLimit: 10, // Adjust this value according to your requirements
   host: 'bxsfkzesct30q9qorkgp-mysql.services.clever-cloud.com',
   user: 'uxwrhngiimlh3yze',
   password: 'DwaVnJPsTDc1hzLuKqSJ',
   database: 'bxsfkzesct30q9qorkgp',
 });
-
 
 
 
